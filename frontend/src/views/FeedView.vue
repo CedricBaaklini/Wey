709a6fa4a0e1385442101dde50596a96882d4793
Cedric Baaklini <cabaaklini@icloud.com>
2024-09-08 14:09:46 -0400

    Initial commit

---
+++ frontend/src/views/FeedView.vue
@@ -0,0 +1,28 @@
+<script>
+
+</script>
+
+<template>
+	<div class="max-w-7xl mx-auto grid grid-cols-4 gap-4">
+		<div class="main-left col-span-1">
+			<div class="p-4 bg-white-border border-gray-200 text-center rounded-lg">
+				<img src = "https://i.pravatar.cc/40?img=70" alt = "profile photo" class="mb-6 rounded-full">
+
+				<p><strong> Cedric Baaklini </strong></p>
+
+				<div class="mt-6 flex space-x-8 justify-around">
+					<p class="text-xs text-gray-500"> 182 friends </p>
+					<p class="text-xs text-gray-500"> 120 posts </p>
+				</div>
+			</div>
+		</div>
+
+		<div class="main-center col-span-2 space-y-4">
+
+		</divc>
+	</div>
+</template>
+
+<style>
+
+</style>
---
+++ frontend/src/views/LoginView.vue
@@ -0,0 +1,37 @@
+<template>
+	<div class="max-w-7xl mx-auto grid grid-cols-2 gap-4">
+		<div class="main-left">
+			<div class="p-12 bg-white border border-gray-200 rounded-lg">
+				<h1 class="mb-6 text-2xl"> Log in </h1>
+
+				<p class="mb-6 text-gray-500">
+					Meliore montes eirmod postulant perpetua morbi ridiculus dapibus referrentur prodesset delenit consul deterruisset scripserit aeque agam reformidans omittam libris fringilla hinc pericula curae fugit curabitur dicat neglegentur deseruisse habitasse elit adhuc elit oratio parturient atqui scelerisque contentiones nisl sit fabellas pulvinar cras postulant eloquentiam mnesarchum partiendo
+				</p>
+
+				<p class="font-bold">
+					Don't have an account? <RouterLink :to="{'name': 'signup'}" class="underline"> Click here </RouterLink> to create one!
+				</p>
+			</div>
+		</div>
+
+		<div class="main-right">
+			<div class="p-12 bg-white border border-gray-200 rounded-lg">
+				<form class="space-y-6">
+					<div>
+						<label> E-mail </label>
+						<input type="email" placeholder="Your email address" class="w-full mt-2 py-4 px-6 border border-gray-200 rounded-lg">
+					</div>
+
+					<div>
+						<label> Password </label>
+						<input type="password" placeholder="Your password" class="w-full mt-2 py-4 px-6 border border-gray-200 rounded-lg">
+					</div>
+
+					<div>
+						<button class="py-4 px-6 bg-purple-600 text-white rounded-lg"> Log in </button>
+					</div>
+				</form>
+			</div>
+		</div>
+	</div>
+</template>
