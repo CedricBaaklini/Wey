0d8070983a3d5b46a4e1078ede50d0d403491b61
Cedric Baaklini <cabaaklini@icloud.com>
2024-10-05 00:35:23 -0400

    Creating profile page.

--- backend/post/api.py
+++ backend/post/api.py
@@ -3,6 +3,8 @@ from rest_framework.decorators import api_view
 from .serializers import PostSerializer
 from .models import Post
 from .forms import PostForm
+from account.models import User
+from account.serializers import UserSerializer
 
 @api_view(['GET'])
 def post_list(request):
@@ -11,6 +13,16 @@ def post_list(request):
     serializer = PostSerializer(posts, many=True)
 
     return JsonResponse(serializer.data, safe=False)
+
+@api_view(['GET'])
+def post_list_profile(request, id):
+    user = User.objects.get(pk=id)
+    posts = Post.objects.filter(created_by_id=id)
+
+    post_serializer = PostSerializer(posts, many=True)
+    user_serializer = UserSerializer(user)
+
+    return JsonResponse({'posts': post_serializer.data, 'user': user_serializer.data}, safe=False)
 
 @api_view(['POST'])
 def post_create(request):
